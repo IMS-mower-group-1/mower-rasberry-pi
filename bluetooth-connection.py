22ff5395e542c7a30b3f90a256295230e763683b
Axel <axeltigerberg@gmail.com>
2023-04-04 10:19:20 +0200

    bluetooth code that worked

--- bluetooth-connection.py
+++ bluetooth-connection.py
@@ -1,48 +1,20 @@
-# file: rfcomm-client.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a client application that uses RFCOMM sockets
-#       intended for use with rfcomm-server
-#
-# $Id: rfcomm-client.py 424 2006-08-24 03:35:54Z albert $
+import bluetooth
 
-from bluetooth import *
-import sys
+# Define the Bluetooth socket parameters
+server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+port = 1 # RFCOMM port number
+server_socket.bind(("", bluetooth.PORT_ANY))
+server_socket.listen(1) # Maximum number of queued connections
 
-if sys.version < '3':
-    input = raw_input
+# Wait for a Bluetooth client to connect
+print("Waiting for a Bluetooth client...")
+client_socket, client_adress = server_socket.accept()
+print(f"Accepted connection from {client_adress}")
 
-addr = None
+# Send data to the client
+#data = "Hello, client!"
+#client_socket.send(data)
 
-if len(sys.argv) < 2:
-    print("no device specified.  Searching all nearby bluetooth devices for")
-    print("the SampleServer service")
-else:
-    addr = sys.argv[1]
-    print("Searching for SampleServer on %s" % addr)
-
-# search for the SampleServer service
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-service_matches = find_service( uuid = uuid, address = addr )
-
-if len(service_matches) == 0:
-    print("couldn't find the SampleServer service =(")
-    sys.exit(0)
-
-first_match = service_matches[0]
-port = first_match["port"]
-name = first_match["name"]
-host = first_match["host"]
-
-print("connecting to \"%s\" on %s" % (name, host))
-
-# Create the client socket
-sock=BluetoothSocket( RFCOMM )
-sock.connect((host, port))
-
-print("connected.  type stuff")
-while True:
-    data = input()
-    if len(data) == 0: break
-    sock.send(data)
-
-sock.close()
+# Close the sockets
+#client_socket.close()
+#server_socket.close()
