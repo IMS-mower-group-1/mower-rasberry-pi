b7b4bafa4fec4af8afadcb3a3fc7b0f434b8e86a
Lasse <lasse.lejon@hotmail.com>
2023-04-04 13:42:31 +0200

    Working bluetooth socket connection.

--- bluetooth-connection.py
+++ bluetooth-connection.py
@@ -1,20 +1,35 @@
-import bluetooth
+from bluetooth import *
 
-# Define the Bluetooth socket parameters
-server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-port = 1 # RFCOMM port number
-server_socket.bind(("", bluetooth.PORT_ANY))
-server_socket.listen(1) # Maximum number of queued connections
+server_socket = BluetoothSocket(RFCOMM)
+server_socket.bind(("",PORT_ANY))
+server_socket.listen(1)
+print("waiting for client to connect...")
 
-# Wait for a Bluetooth client to connect
-print("Waiting for a Bluetooth client...")
-client_socket, client_adress = server_socket.accept()
-print(f"Accepted connection from {client_adress}")
+port = server_socket.getsockname()[1]
 
-# Send data to the client
-#data = "Hello, client!"
-#client_socket.send(data)
+uuid = "30097c35-95b9-4f92-9d2e-e3e06aa3b07f"
 
-# Close the sockets
-#client_socket.close()
-#server_socket.close()
+advertise_service(server_socket,
+"PleaseWork",
+service_id=uuid,
+service_classes = [uuid, SERIAL_PORT_CLASS],
+profiles = [SERIAL_PORT_PROFILE]
+)
+
+
+print("asdfasdf")
+client_sock, client_info = server_socket.accept()
+print(client_info)
+
+
+while True:
+	print("test")
+	data = client_sock.recv(1024)
+	if len(data) == 0: break
+	print(data)
+
+print("disc")
+
+client_sock.close()
+server_socket.close()
+
