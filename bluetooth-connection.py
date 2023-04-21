90179e1432568ad1ee802894481826301eb7e31e
phille923 <philleabraham@hotmail.com>
2023-04-21 12:44:17 +0100

    Fixed serial code to work USB0

--- bluetooth-connection.py
+++ bluetooth-connection.py
@@ -1,7 +1,12 @@
+import sys
+sys.path.append('/home/admin/.local/lib/python3.9/site-packages')
+
 from bluetooth import *
+import serial
+import time
 
 server_socket = BluetoothSocket(RFCOMM)
-server_socket.bind(("",PORT_ANY))
+server_socket.bind(("", PORT_ANY))
 server_socket.listen(1)
 print("waiting for client to connect...")
 
@@ -12,24 +17,24 @@ uuid = "30097c35-95b9-4f92-9d2e-e3e06aa3b07f"
 advertise_service(server_socket,
 "PleaseWork",
 service_id=uuid,
-service_classes = [uuid, SERIAL_PORT_CLASS],
-profiles = [SERIAL_PORT_PROFILE]
+service_classes=[uuid, SERIAL_PORT_CLASS],
+profiles=[SERIAL_PORT_PROFILE]
 )
-
 
 print("asdfasdf")
 client_sock, client_info = server_socket.accept()
 print(client_info)
 
-
-while True:
-	data = client_sock.recv(1024).decode('utf-8')
-	if len(data) == 0: break
-	print(data)
-	client_sock.send("Pi4 received message: "+data)
-
-print("disc")
-
-client_sock.close()
-server_socket.close()
-
+try:
+    ser = serial.Serial('/dev/ttyUSB0', 9600)
+    while True:
+        data = client_sock.recv(1024).decode('utf-8')
+        if len(data) == 0: break
+        ser.write(data.encode())
+        print(data)
+        client_sock.send('1')
+finally:
+    print("disc")
+    client_sock.close()
+    ser.close()
+    server_socket.close()
