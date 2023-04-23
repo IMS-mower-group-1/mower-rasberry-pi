f23d3c7b24c0de23f7863cc5ea7617048906a2fe
phille923 <philleabraham@hotmail.com>
2023-04-23 08:54:48 +0100

    Sending position data to arduino works correctly

--- bluetooth-connection.py
+++ bluetooth-connection.py
@@ -5,36 +5,38 @@ from bluetooth import *
 import serial
 import time
 
-server_socket = BluetoothSocket(RFCOMM)
-server_socket.bind(("", PORT_ANY))
-server_socket.listen(1)
-print("waiting for client to connect...")
-
-port = server_socket.getsockname()[1]
-
-uuid = "30097c35-95b9-4f92-9d2e-e3e06aa3b07f"
-
-advertise_service(server_socket,
-"PleaseWork",
-service_id=uuid,
-service_classes=[uuid, SERIAL_PORT_CLASS],
-profiles=[SERIAL_PORT_PROFILE]
-)
-
-print("asdfasdf")
-client_sock, client_info = server_socket.accept()
-print(client_info)
-
-try:
+while True:
     ser = serial.Serial('/dev/ttyUSB0', 9600)
+    server_socket = BluetoothSocket(RFCOMM)
+    server_socket.bind(("", PORT_ANY))
+    server_socket.listen(1)
+    print("waiting for client to connect...")
+
+    port = server_socket.getsockname()[1]
+
+    uuid = "30097c35-95b9-4f92-9d2e-e3e06aa3b07f"
+
+    advertise_service(server_socket,
+    "PleaseWork",
+    service_id=uuid,
+    service_classes=[uuid, SERIAL_PORT_CLASS],
+    profiles=[SERIAL_PORT_PROFILE]
+    )
+
+    print("asdfasdf")
+    client_sock, client_info = server_socket.accept()
+    print(client_info)
+
     while True:
-        data = client_sock.recv(1024).decode('utf-8')
-        if len(data) == 0: break
-        ser.write(data.encode())
-        print(data)
-        client_sock.send('1')
-finally:
-    print("disc")
-    client_sock.close()
-    ser.close()
-    server_socket.close()
+        try:
+            data = client_sock.recv(1024).decode('utf-8')
+            if len(data) == 0: break
+            print(data.encode())
+            ser.write(data.encode())
+        except BluetoothError as e:
+            print(f"Error occurred: {e}")
+            print("disconnecting..")
+            client_sock.close()
+            ser.close()
+            server_socket.close()
+            break
